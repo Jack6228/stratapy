93dd0fe21526a7c292f6a357a9a76647deccf480
Jack6228 <jackleesmith100@gmail.com>
2026-05-25 13:52:28 +0100

    CI refactoring

--- noxfile.ci.py
+++ noxfile.ci.py
@@ -4,9 +4,11 @@ import nox
 @nox.session(python=False)
 def ci(session):
     # Install dependency versions
-    session.install("numpy==2.0.*", "pandas==2.2.*", "matplotlib>=3.9")
+    # session.install("numpy==2.0.*", "pandas==2.2.*", "matplotlib>=3.9")
+    session.run("pip", "install", "numpy==2.0.*", "pandas==2.2.*", "matplotlib>=3.9", external=True)
     # install package and test tooling
-    session.install(".", "pytest")
+    # session.install(".", "pytest")
+    session.run("pip", "install", ".", "pytest", external=True)
     # Smoke-check
     session.run("python", "-c", "import stratapy, sys; print(stratapy.__version__)")
     # Run tests
